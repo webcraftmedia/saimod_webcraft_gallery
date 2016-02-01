62ccd8676c03a064247a98494c331a1571b6737d
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-02-01 19:59:35 +0100

    made images responsive

--- saimod_webcraft_gallery.php
+++ saimod_webcraft_gallery.php
@@ -16,7 +16,7 @@ class saimod_webcraft_gallery extends \SYSTEM\SAI\SaiModule {
         $gallery = \SQL\SAIMOD_WEBCRAFT_GALLERY_GALLERY_ID::QQ(array($id));
         $result = '';
         while($img = $gallery->next()){
-            $result .= '<img class="carousel-img" src="./api.php?call=files&cat='.$img['file_cat'].'&id='.$img['file_id'].'" alt="">';}
+            $result .= '<img class="carousel-img img-responsive" src="./api.php?call=files&cat='.$img['file_cat'].'&id='.$img['file_id'].'" alt="">';}
         $result .= '<script>Galleria.loadTheme(\''.\SYSTEM\WEBPATH(new PSAI(), 'saimod_webcraft_gallery/js/galleria/themes/classic/galleria.classic.min.js').'\');Galleria.run(\'.galleria\');</script>';
         return $result;
     }
--- tpl/saimod_webraft_gallery_gallery_item.tpl
+++ tpl/saimod_webraft_gallery_gallery_item.tpl
@@ -1,5 +1,5 @@
 <div class="item ${active}">
-    <img class="carousel-img" src="./api.php?call=files&cat=${file_cat}&id=${file_id}" alt="">
+    <img class="carousel-img img-responsive" src="./api.php?call=files&cat=${file_cat}&id=${file_id}" alt="">
     <div class="carousel-caption">
         <h4>${heading}</h4>
         <p>${description}</p>
